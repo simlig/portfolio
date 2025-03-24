ad5bc90eec9fd7d268f3fe1bcf4246807b06a69e
simlig <samuelimlig@gmail.com>
2025-03-23 21:37:51 -0700

    Refine Navbar

--- src/app/components/Navbar/Narbar.tsx
+++ src/app/components/Navbar/Narbar.tsx
@@ -1,20 +1,22 @@
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 import React from 'react';
 
 
 const Navbar = () => {
     return (
-        <nav className="bg-blue-600 p-4 sticky top-0 z-10">
-        <div className="mx-auto flex items-center justify-between">
-
-
-            <div className="flex">
-                <a href="/" className="text-white px-4 rounded focus:outline-none focus:ring-4 focus:ring-white border-1 border-transparent hover:border-white transition-all">Home</a>
-                <a href="/PDFs/Samuel Imlig Resume.pdf" className="text-white px-4 rounded focus:outline-none focus:ring-4 focus:ring-white border-1 border-transparent hover:border-white transition-all">Resume</a>
-                <a href="#" className="text-white px-4 rounded focus:outline-none focus:ring-4 focus:ring-white border-1 border-transparent hover:border-white transition-all">About</a>
-                
+        <nav className="bg-blue-700 p-4 sticky top-0 z-10 border-b-1 border-b-gray-500">
+            <div className="mx-auto flex items-center justify-between">
+                <div className="flex">
+                    <Link href="/" className="text-white px-4 rounded focus:outline-none focus:ring-4 focus:ring-white border-1 border-transparent hover:border-white transition-all">Home</Link>
+                    <Link href="/PDFs/Samuel Imlig Resume.pdf" className="text-white px-4 rounded focus:outline-none focus:ring-4 focus:ring-white border-1 border-transparent hover:border-white transition-all">Resume</Link>
+                    <Link href="#" className="text-white px-4 rounded focus:outline-none focus:ring-4 focus:ring-white border-1 border-transparent hover:border-white transition-all">About</Link>
+                    <Link href = "#" className="text-white px-4 rounded focus:outline-none focus:ring-4 focus:ring-white border-1 border-transparent hover:border-white z-20">
+                        <FontAwesomeIcon icon={faLinkedin} className={"h-6"} />
+                    </Link>
+                </div>
             </div>
-
-        </div>
         </nav>
     )
 }
