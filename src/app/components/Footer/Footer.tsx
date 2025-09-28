3338441f5bf6c672495f904ce3a776d49fdbb540
simlig <samuelimlig@gmail.com>
2025-09-27 19:31:29 -0700

    Add email button

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../Footer/Footer.module.css";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
@@ -18,6 +19,9 @@ const Footer = () => {
           </a>
           <a href="https://github.com/simlig/portfolio" target="_blank" rel="noopener noreferrer" className={styles.links}>
             <FontAwesomeIcon icon={faGithub} className="fa-2x" />
+          </a>
+          <a href="mailto:samuelimlig@gmail.com" className={styles.links}>
+            <FontAwesomeIcon icon={faEnvelope} className="fa-2x" />
           </a>
         </div>
 
